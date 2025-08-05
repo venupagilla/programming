15f4bd7d5a5c61037b939f43a75ee4d25e73b06e
venu pagilla <94222926+venupagilla@users.noreply.github.com>
2025-08-06 00:31:13 +0530

    solved trailing zeros problem in cses

---
+++ CSES/trailingZeros.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class trailingZeros {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int zeros=0;
+        for(int i=5;i<=n;i*=5){
+            zeros+=n/i;
+        }
+        System.out.println(zeros);
+    }
+}
