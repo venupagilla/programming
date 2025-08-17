d42621fc14c1e81de534a669212b7f4f00e7d41e
venu pagilla <94222926+venupagilla@users.noreply.github.com>
2025-08-17 23:07:22 +0530

    web day2

---
+++ CSES/creatingStrings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class creatingStrings {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String hi=sc.next();
+        char[] arr=hi.toCharArray();
+        System.out.println(hi.length()*(hi.length()-1));
+        for()
+    }
+}
